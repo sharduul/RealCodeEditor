f8bb4d31ad4c33cdead5ad72fcdc1aa86567752c
Shardul Bagade <bagadeshardul@gmail.com>
2014-04-19 10:54:21 -0400

    bug fixing: user1 could not run program when user2 changed the code

--- program_to_run.c
+++ program_to_run.c
@@ -2,5 +2,11 @@
 
 void main()
 {
-printf("helloworldyyyyyxxxx\n");
+printf("ellxx\n");
+printf("ellxx\n");
+printf("ellxx\n");
+
+int c = 3 + 4;
+
+printf("%d", c);
 }
--- public/js/controllers/mainController.js
+++ public/js/controllers/mainController.js
@@ -4,16 +4,19 @@ realCodeEditor.controller('mainController',
 	function mainController($scope, runCodeService){
 
 		// initialize the variables
-		$scope.code = "";
+		//$scope.code = "";
 		$scope.output = "";
+		
+		
 		
 		// function to run code
 		$scope.runCode = function(){
 		
 			$scope.output = ""; // initialize the output box
+			var code = document.getElementById("sharetext").value; // get value of text area actually
 		
 			// encode to escape the special characters
-			return runCodeService.runCode(escape($scope.code))
+			return runCodeService.runCode(escape(code))
 				.success(function(data) {
 					$scope.output = data.output_text;
 				});
